4fd8bcfa74b9a8ee51e348f7b9331033567a1192
Denz <dennz78@gmail.com>
2014-11-24 18:49:35 +0200

    request headers based routing
    match_headers route string helper
    HeadersBasedRule rule class

--- ldp/resource.py
+++ ldp/resource.py
@@ -1,13 +1,20 @@
 from itertools import chain
 from collections import OrderedDict
 from functools import wraps
+from threading import Lock
+from werkzeug.routing import Map, parse_rule, parse_converter_args
+import re
 from flask import (Blueprint, Flask, request, current_app, g)
+from flask.ext.negotiation import provides
+from flask.templating import _default_template_ctx_processor
 from rdflib import Graph
 from rdflib.namespace import RDF
 from treelib import Tree, Node
 
 
 from ldp import NS as LDP
+from ldp.rule import HeadersRule, match_headers
+
 
 def ldp_types_hierarchy():
     h = Tree()
@@ -28,7 +35,7 @@ def subclasslist(base):
     return [base, ] + list(chain(*subs))
 
 
-def resource_builder(tree, node, *args, debug=False, **kwargs):
+def resource_builder(tree, node, *args, debug=True, **kwargs):
     '''
     Maps rdflib.Resource to ldp.Resource app
     '''
@@ -36,20 +43,14 @@ def resource_builder(tree, node, *args, debug=False, **kwargs):
                       in node.data.objects(RDF.type)
                       if r.identifier.startswith(LDP)]
 
-    app = Flask(*args, **kwargs)
+    app = get_resource_class(*resource_types)(node, *args, **kwargs)
 
     app.debug = debug
-
-    @app.after_request
-    def set_ldp_link_types(response):
-
-        response.headers['Link'] = '\t,'.join(('<%s>; rel="type"' % r_type
-                                               for r_type in resource_types))
-        return response
 
     app.logger.debug('Built %s' % app)
 
     return app
+
 
 def get_resource_class(*ldp_types, hierarchy=TYPES):
     resource_types = reversed(subclasslist(Resource))
@@ -57,6 +58,7 @@ def get_resource_class(*ldp_types, hierarchy=TYPES):
     for resource_type in resource_types:
         if resource_type.ldp_type == deepest_type:
             return resource_type
+
 
 def implied_types(*explicit_types, hierarchy=TYPES):
     implicit_types = []
@@ -70,8 +72,28 @@ def implied_types(*explicit_types, hierarchy=TYPES):
                     implicit_types.append(implicit_type)
 
 
+
+
 class Resource(Flask):
     ldp_type = LDP.Resource
+    url_rule_class = HeadersRule
+    def __init__(self, node, *args, **kwargs):
+        super(Resource, self).__init__(*args, **kwargs)
+        self.node = node
+
+    def create_url_adapter(self, request):
+        adapter = super(Resource, self).create_url_adapter(request)
+        if request is not None:
+            for rule in adapter.map._rules:
+                rule.headers = request.headers
+
+            def match(self, *args, **kwargs):
+                rv = adapter.match(*args, **kwargs)
+                for rule in adapter.map._rules:
+                    del rules.__dict__['headers']
+                return rv
+            adapter.match = adapter.match.__get__(adapter, adapter.__class__)
+        return adapter
 
 
 class RDFSource(Resource):
---
+++ ldp/rule.py
@@ -0,0 +1,99 @@
+from flask import Flask
+from werkzeug.routing import (Map,
+                              parse_rule,
+                              parse_converter_args,
+                              RequestAliasRedirect,
+                              ValidationError)
+from werkzeug._compat import iteritems
+import fnmatch
+import re
+
+
+class match_headers(str):
+    __slots__ = ('headers', )
+
+    def __new__(cls, path, **headers):
+        ret = str.__new__(cls, path)
+        ret.headers = headers
+        return ret
+
+
+class HeadersRule(Flask.url_rule_class):
+
+    def match(self, *args, **kwargs):
+        match = super(HeadersRule, self).match(*args, **kwargs)
+        if hasattr(self.rule, 'headers') and match is not None:
+            if not hasattr(self, '_header_rules_compiled'):
+                self.rule.headers = dict(((k, self.compile_header_rule(v))
+                                          for k, v in self.rule.headers.items()))
+                self._header_rules_compiled = True
+            if not set(self.headers.keys()).issuperset(set(self.rule.headers.keys())):
+                return None
+
+            for header in self.rule.headers:
+                header_match = self.rule.headers[
+                    header].search(self.headers[header])
+                if not header_match:
+                    return None
+                groups = header_match.groupdict()
+                result = {}
+                for name, value in iteritems(groups):
+                    try:
+                        value = self._converters[name].to_python(value)
+                    except ValidationError:
+                        return
+                    result[str(name)] = value
+                if self.defaults:
+                    result.update(self.defaults)
+
+                if self.alias and self.map.redirect_defaults:
+                    raise RequestAliasRedirect(result)
+                match.update(result)
+
+        return match
+
+    def compile_header_rule(self, rule):
+        """Compiles the regular expression and stores it."""
+        assert self.map is not None, 'rule not bound'
+        if not '_trace' in self.__dict__:
+            self._trace = []
+        if not '_converters' in self.__dict__:
+            self._converters = {}
+        if not '_weights' in self.__dict__:
+            self._weights = []
+        if not 'arguments' in self.__dict__:
+            self.arguments = set()
+        regex_parts = []
+
+        def _build_regex(rule):
+            for converter, arguments, variable in parse_rule(rule):
+                if converter is None:
+                    regex_parts.append(fnmatch.translate(variable)[:-7])
+                    self._trace.append((False, variable))
+                    for part in variable.split('/'):
+                        if part:
+                            self._weights.append((0, -len(part)))
+                else:
+                    if arguments:
+                        c_args, c_kwargs = parse_converter_args(arguments)
+                    else:
+                        c_args = ()
+                        c_kwargs = {}
+                    convobj = self.get_converter(
+                        variable, converter, c_args, c_kwargs)
+                    regex_parts.append(
+                        '(?P<%s>%s)' % (variable, convobj.regex))
+                    self._converters[variable] = convobj
+                    self._trace.append((True, variable))
+                    self._weights.append((1, convobj.weight))
+                    self.arguments.add(str(variable))
+
+        # _build_regex(domain_rule)
+        # regex_parts.append('\\|')
+        self._trace.append((False, '|'))
+        _build_regex(rule)
+        self._trace.append((False, '/'))
+        regex = r'^%s$' % (
+            u''.join(regex_parts)
+        )
+        return re.compile(regex, re.UNICODE)
