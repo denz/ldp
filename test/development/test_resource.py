ef6740e8bc9d6ae2d67d2760f7f6851e721f57e7
Denz <dennz78@gmail.com>
2014-12-07 21:12:53 +0200

    test refactoring

--- test/development/test_resource.py
+++ test/development/test_resource.py
@@ -13,52 +13,52 @@ class TestResource(LdpTest):
     `http://www.w3.org/TR/ldp/#h3_ldpr-resource`_
     """
 
-    # def test_options(self):
-    #     for url in self.urls():
-    #         response = self.c.open(url, method='OPTIONS')
-    #         self.assertTrue(response.status_code < 500)
+    def test_options(self):
+        for url in self.urls():
+            response = self.c.open(url, method='OPTIONS')
+            self.assertTrue(response.status_code < 500)
 
-    # def test_link_header(self):
-    #     response = self.c.open('http://example.org/')
-    #     self.assertIn(LDP.Resource, response.headers['Link'])
-    #     self.assertIn(LDP.RDFSource, response.headers['Link'])
+    def test_link_header(self):
+        response = self.c.open('http://example.org/')
+        self.assertIn(LDP.Resource, response.headers['Link'])
+        self.assertIn(LDP.RDFSource, response.headers['Link'])
 
-    #     for url in ('http://example.org/alice',
-    #                 'http://example.org/den',
-    #                 'http://example.org/alice',):
-    #         response = self.c.open(url)
-    #         self.assertIn(LDP.Resource, response.headers['Link'])
-    #         self.assertIn(LDP.RDFSource, response.headers['Link'])
-    #         self.assertIn(LDP.Container, response.headers['Link'])
-    #         self.assertIn(LDP.BasicContainer, response.headers['Link'])
-    #     print(scheme.serialize(format='turtle').decode())
+        for url in ('http://example.org/alice',
+                    'http://example.org/den',
+                    'http://example.org/alice',):
+            response = self.c.open(url)
+            self.assertIn(LDP.Resource, response.headers['Link'])
+            self.assertIn(LDP.RDFSource, response.headers['Link'])
+            self.assertIn(LDP.Container, response.headers['Link'])
+            self.assertIn(LDP.BasicContainer, response.headers['Link'])
+        print(scheme.serialize(format='turtle').decode())
 
-    # def test_append_implied_types(self):
-    #     self.assertEqual(set(implied_types(LDP.BasicContainer)),
-    #                      set([LDP.Resource,
-    #                           LDP.Container,
-    #                           LDP.RDFSource, ]))
+    def test_append_implied_types(self):
+        self.assertEqual(set(implied_types(LDP.BasicContainer)),
+                         set([LDP.Resource,
+                              LDP.Container,
+                              LDP.RDFSource, ]))
 
-    # def test_resource_class_getter(self):
-    #     self.assertEqual(get_resource_class(LDP.Resource,
-    #                                         LDP.RDFSource,
-    #                                         LDP.Container,
-    #                                         LDP.BasicContainer),
-    #                      BasicContainer
-    #                      )
-    #     self.assertEqual(get_resource_class(LDP.Resource,
-    #                                         LDP.RDFSource,
-    #                                         LDP.Container),
-    #                      Container
-    #                      )
+    def test_resource_class_getter(self):
+        self.assertEqual(get_resource_class(LDP.Resource,
+                                            LDP.RDFSource,
+                                            LDP.Container,
+                                            LDP.BasicContainer),
+                         BasicContainer
+                         )
+        self.assertEqual(get_resource_class(LDP.Resource,
+                                            LDP.RDFSource,
+                                            LDP.Container),
+                         Container
+                         )
 
-    #     self.assertEqual(get_resource_class(LDP.Resource,
-    #                                         LDP.RDFSource,),
-    #                      RDFSource
-    #                      )
-    #     # self.assertEqual(get_resource_class(LDP.Resource,),
-    #     #                  Resource
-    #     #                  )
+        self.assertEqual(get_resource_class(LDP.Resource,
+                                            LDP.RDFSource,),
+                         RDFSource
+                         )
+        # self.assertEqual(get_resource_class(LDP.Resource,),
+        #                  Resource
+        #                  )
 
     def test_of_test(self):
         # response = self.c.get('/den', headers={'Accept':'text/turtle'})
